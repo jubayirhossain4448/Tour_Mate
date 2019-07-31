0d12924c656df1cb987dbdbbc04e85f92945a871
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-01 00:10:25 +0600

    EditText

--- TourMate/app/src/main/java/com/jubayir/tourmate/login/SigninActivity.java
+++ TourMate/app/src/main/java/com/jubayir/tourmate/login/SigninActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.jubayir.tourmate.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class SigninActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signin);
     }
 }
