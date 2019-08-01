e6399bd9f64f6aadcfc86bca523768a409c49af6
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-01 15:56:46 +0600

    Next

--- TourMate/app/src/main/java/com/jubayir/tourmate/MainActivity.java
+++ TourMate/app/src/main/java/com/jubayir/tourmate/MainActivity.java
@@ -2,13 +2,22 @@ package com.jubayir.tourmate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.jubayir.tourmate.login.SigninActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void next(View view) {
+        startActivity(new Intent(MainActivity.this, SigninActivity.class));
     }
 }
