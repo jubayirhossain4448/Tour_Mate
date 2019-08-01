f1e3355c73c66352e9dbfb5ec2efcd22abd41d2b
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-01 12:47:40 +0600

    Signup_activity

---
+++ TourMate/app/src/main/java/com/jubayir/tourmate/login/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.jubayir.tourmate.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jubayir.tourmate.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
