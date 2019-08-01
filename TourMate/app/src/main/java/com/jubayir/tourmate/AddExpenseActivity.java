618a811a68201b66299f4004d9ceb6e1cacfc95b
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-02 00:44:32 +0600

    AddExpenseActivity

---
+++ TourMate/app/src/main/java/com/jubayir/tourmate/AddExpenseActivity.java
@@ -0,0 +1,14 @@
+package com.jubayir.tourmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+    }
+}
